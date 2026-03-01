4637cd0365909f221785e95da6908b84b1aad018
Iuky.O <iumyfarias03@gmail.com>
2026-02-28 23:07:03 -0300

    fix: ajuste do topbar

--- lib/widgets/custom_topbar.dart
+++ lib/widgets/custom_topbar.dart
@@ -5,41 +5,44 @@ class CustomTopBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomTopBar({super.key});
 
   @override
-  Size get preferredSize => const Size.fromHeight(40);
+  Size get preferredSize => const Size.fromHeight(60);
 
   @override
   Widget build(BuildContext context) {
+    const Color verdeEscuro = Color(0xFF356B33);
+
     return AppBar(
       backgroundColor: AppColors.primaryGreen,
       elevation: 0,
+      // 1. REMOVE A SETA DE VOLTAR
+      automaticallyImplyLeading: false, 
       flexibleSpace: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Align(
             alignment: Alignment.centerLeft,
             child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 5,
-                vertical: 5,
-              ),
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: const Color(0xFFFDFBF0),
+                border: Border.all(color: verdeEscuro, width: 1.5), 
                 borderRadius: BorderRadius.circular(25),
               ),
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: const [
                   Icon(
-                    Icons.location_on,
-                    size: 20,
-                    color: AppColors.primaryGreen,
+                    Icons.eco, 
+                    size: 18, 
+                    color: verdeEscuro,
                   ),
-                  SizedBox(width: 3),
+                  SizedBox(width: 6),
                   Text(
-                    "Egua da Feira",
+                    "Égua da Feira",
                     style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: verdeEscuro,
                     ),
                   ),
                 ],
